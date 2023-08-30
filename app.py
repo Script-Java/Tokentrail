93a5d48df5f318d58ef80ca849edc9dd8c47e057
Script-Java <atrindev@gmail.com>
2023-08-30 13:25:37 -0500

    finished

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request
+import pretty_errors
 from scrape import CoinMarket
+from urllib.parse import unquote
 
 coinmarket = CoinMarket()
 
 app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smpt.gmail.com'
+app.config['secret_key'] = 'wbqeugofbdasl3jk@b4308597jwhbdfksjdb092125sdf'
 
 @app.route('/')
 def index():
@@ -19,6 +23,34 @@ def assets():
 def about():
     
     return render_template('about.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    try:
+        search_input = request.form.get('search_input')
+        output_data = coinmarket.search_crypto(str(search_input))
+        return render_template('search.html', output=output_data)
+    except KeyError:
+        return render_template('error.html')
+    
+@app.route('/search/<token_symbol>', methods=['GET', 'POST'])
+def search_symbol(token_symbol):
+    try:
+        input_str = str(token_symbol)
+        output = unquote(input_str).lower()
+        search_input = output.replace(' ', '-')
+        print(search_input)
+        output_data = coinmarket.search_crypto(search_input)
+        if output_data is None:
+            raise Exception(f'No data was found for {search_input}')
+        return render_template('search.html', output=output_data)
+    
+    except KeyError:
+        return render_template('error.html')
+    
+@app.route('/thankyou')
+def feedback_submit():
+    return render_template('thankyou.html')
 
 @app.route('/feedback')
 def contact():
--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class CoinMarket:
     def __init__(self):
@@ -88,5 +89,58 @@ class CoinMarket:
             return crypto_list   
         except Exception as e:
             return e
+        
+        
+    def search_crypto(self, name):
+        url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
+        params = {
+            'slug': name,
+            'convert': 'USD'
+        }
+        req = requests.get(url, params=params, headers=self.headers)
+        res = req.json()
+        
+        
+        current_crypto = list(res['data'].values())[0]
+        name = current_crypto['name']
+        symbol = current_crypto['symbol']
+        price = current_crypto['quote']['USD']['price']
+        volume_24h = current_crypto['quote']['USD']['volume_24h']
+        volume_change_24h = current_crypto['quote']['USD']['volume_change_24h']
+        percent_change_1h = current_crypto['quote']['USD']['percent_change_1h']
+        percent_change_24h = current_crypto['quote']['USD']['percent_change_24h']
+        percent_change_7d = current_crypto['quote']['USD']['percent_change_7d']
+        percent_change_30d = current_crypto['quote']['USD']['percent_change_30d']
+        percent_change_60d = current_crypto['quote']['USD']['percent_change_60d']
+        percent_change_90d = current_crypto['quote']['USD']['percent_change_90d']
+        market_cap = current_crypto['quote']['USD']['market_cap']
+    
+        logo = f'https://coinicons-api.vercel.app/api/icon/{symbol.lower()}'
+        search_data = {
+            'name': name,
+            'symbol': symbol,
+            'price': round(price,2),
+            'logo': logo,
+            'volume_24h': round(volume_24h,2),
+            'volume_24h_change': round(volume_change_24h,2),
+            'percent_change_1h': round(percent_change_1h,2),
+            'percent_change_24h': round(percent_change_24h,2),
+            'percent_change_7d':round(percent_change_7d, 2),
+            'percent_change_30d':round(percent_change_30d,2),
+            'percent_change_60d':round(percent_change_60d,2),
+            'percent_change_90d':round(percent_change_90d,2),
+            'market_cap': round(market_cap,2)
+        }
+
+        #return search_data
+        return search_data
+        
+        
+        
+
+
+
+
+
 
 
